46a612eb58284000bf97ab35e9ae0d5b53bc6457
James Jacob <jamesjoboyjacob@gmail.com>
2026-08-11 15:12:06 -0400

    made revision to cookie handling

--- server/config/session.js
+++ server/config/session.js
@@ -28,7 +28,7 @@ export function createSessionMiddleware() {
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
-      sameSite: isProduction ? "none" : "lax",
+      sameSite: "lax",
       secure: isProduction,
     },
   });
